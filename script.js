bc7bfe359f55f5cbcedf163ead4e778e5e8a3819
snit902 <9028182@student.zadkine.nl>
2025-12-05 15:05:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+<script>
+  const toggleBtn = document.getElementById('dark-mode-toggle');
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    // Optioneel: onthoud de voorkeur in cookies/localStorage
+    if (document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('darkMode', 'enabled');
+    } else {
+      localStorage.setItem('darkMode', 'disabled');
+    }
+  });
+
+  // Laad voorkeur bij pagina load
+  window.onload = () => {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+      document.body.classList.add('dark-mode');
+    }
+  }
+</script>
